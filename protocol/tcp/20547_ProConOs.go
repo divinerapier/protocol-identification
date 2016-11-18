06804449a3d05b2fb17d668c99c280a39d1a1c4e
Divine <poriter.coco@gmail.com>
2016-11-18 14:24:06 +0800

    add grab proconos protocol

--- protocol/tcp/20547_ProConOs.go
+++ protocol/tcp/20547_ProConOs.go
@@ -2,24 +2,28 @@ package tcp
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net"
 	"time"
 )
 
-func SendProconos(addr string) {
+// commit
+
+// GrabProConOs grab proconos
+func GrabProConOs(addr string) (map[string]string, error) {
 	keys := []string{"Ladder Logic Runtime", "PLC Type", "Project Name", "Boot Project", "Project Source Code"}
 	m := make(map[string]string)
 	conn, err := net.DialTimeout("tcp", addr, time.Second*3)
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return nil, err
 	}
 	conn.Write([]byte("\xcc\x01\x00\x0b\x40\x02\x00\x00\x47\xee"))
 	buf := make([]byte, 1024)
 	cnt, _ := conn.Read(buf)
 	if buf[0] != '\xcc' {
-		return
+		return nil, errors.New("not proconos")
 	}
 	buf = buf[12:cnt]
 	buf = bytes.Replace(buf, []byte("\x00\x00"), []byte("\x00"), -1)
@@ -38,4 +42,5 @@ func SendProconos(addr string) {
 	for k, v := range m {
 		fmt.Printf("%s: %s\n", k, v)
 	}
+	return m, nil
 }
